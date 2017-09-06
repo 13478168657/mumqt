1ebcb232350e524ac42d604f42c746c4eaa507c6
ykk <ykk@qq.com>
2017-09-06 23:53:00 +0800

    sfsa

--- app/Http/Controllers/WebChat/WebChatController.php
+++ app/Http/Controllers/WebChat/WebChatController.php
@@ -2,20 +2,52 @@
 namespace App\Http\Controllers\WebChat;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-$res = new WebChatController();
-$res->responseMsg();
 class WebChatController extends Controller{
     const token = 'ykk123';
     public function check(Request $request){
-        $timestamp = $request->input('timestamp');
-        $nonce = $request->input('nonce');
-        $echostr = $request->input('echostr');
-        $signature = $request->input('signature');
-        $arr = [$timestamp,$nonce,self::token];
-        sort($arr);
-        $str = implode('',$arr);
-        if(sha1($str) == $signature){
-            echo $echostr;
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        if (!empty($postStr)){
+            libxml_disable_entity_loader(true);
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+                       <ToUserName><![CDATA[%s]]></ToUserName>
+                       <FromUserName><![CDATA[%s]]></FromUserName>
+                       <CreateTime>%s</CreateTime>
+                       <MsgType><![CDATA[%s]]></MsgType>
+                       <Content><![CDATA[%s]]></Content>
+                       <FuncFlag>0</FuncFlag>
+                       </xml>";
+            //订阅事件
+            if($postObj->Event=="subscribe")
+            {
+                $msgType = "text";
+                $contentStr = "欢迎关注安子尘，微信babyanzichen";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }
+            //语音识别
+            if($postObj->MsgType=="voice"){
+                $msgType = "text";
+                $contentStr = trim($postObj->Recognition,"。");
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo  $resultStr;
+            }
+            //自动回复
+            if(!empty( $keyword ))
+            {
+                $msgType = "text";
+                $contentStr = "小朋友你好！";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }else{
+                echo "Input something...";
+            }
+        }else {
+            echo "";
             exit;
         }
     }
