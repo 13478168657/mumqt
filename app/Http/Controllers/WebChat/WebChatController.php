1cc94e41dc103938c4ee5ed1eb565293b6fbd178
ykk <ykk@qq.com>
2017-09-06 23:41:13 +0800

    sfa

--- app/Http/Controllers/WebChat/WebChatController.php
+++ app/Http/Controllers/WebChat/WebChatController.php
@@ -2,6 +2,8 @@
 namespace App\Http\Controllers\WebChat;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+$res = new WebChatController();
+$res->responseMsg();
 class WebChatController extends Controller{
     const token = 'ykk123';
     public function check(Request $request){
@@ -20,5 +22,38 @@ class WebChatController extends Controller{
     public function create(Request $request){
         echo 'sdfs';
         exit;
+    }
+    public function responseMsg()
+    {
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        if (!empty($postStr)){
+            libxml_disable_entity_loader(true);
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+  <ToUserName><![CDATA[%s]]></ToUserName>
+  <FromUserName><![CDATA[%s]]></FromUserName>
+  <CreateTime>%s</CreateTime>
+  <MsgType><![CDATA[%s]]></MsgType>
+  <Content><![CDATA[%s]]></Content>
+  <FuncFlag>0</FuncFlag>
+  </xml>";
+            if(!empty( $keyword ))
+            {
+                $msgType = "text";
+                $contentStr = "Welcome to wechat world!";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }else{
+                echo "Input something...";
+            }
+
+        }else {
+            echo "";
+            exit;
+        }
     }
 }
