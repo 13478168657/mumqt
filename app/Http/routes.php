d3cf7a2354027670aa46b86db9cfa190b9457938
ykk <ykk@qq.com>
2017-09-14 22:36:57 +0800

    sfs

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,5 +10,5 @@ Route::get('article/create','Article\ArticleController@create');
 Route::post('article/postCreate','Article\ArticleController@postCreate');
 Route::post('webChat/check','WebChat\WebChatController@check');
 Route::match(['get', 'post'],'webChat/index','WebChat\WebChatController@index');
-Route::get('weixin/index','web\WebXinController@index');
+Route::get('weixin/index','webChat\WebXinController@index');
 Route::get('menu/create','WebChat\WebChatController@create');
